dc90d2a3f1a9e4d6072ec730262042125e09e561
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-17 17:42:52 -0800

    task 3 C Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - a program to print alphabets in lowercase
+ * and uppercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	/* print aphabets in uppercase*/
+
+		for (c = 'A'; c <= 'Z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	return (0);
+
