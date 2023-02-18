c1e44af6e2c779630f5d45479c3bfe3853553b3c
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-17 16:53:42 -0800

    task 2 c variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,13 @@
 
 int main(void)
 {
-    char Alp[26] = "abcdefghijklmnopqrstuvwxyz";
-    int c;
-    for (c = 0; c < 26; c++)
-    {
-        putchar(Alp[c]);
-    }
-    putchar("\n");
-    return (0);
+	char Alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int c;
+
+	for (c = 0; c < 26; c++)
+	{
+		putchar(Alp[c]);
+	}
+	putchar("Alp\n");
+	return (0);
 }
