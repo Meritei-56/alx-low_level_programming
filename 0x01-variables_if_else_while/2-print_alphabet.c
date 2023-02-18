23c9c3228a0cc7c9a3f38efcc945106d4653e4ed
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-17 16:30:24 -0800

    task 2 C -Variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program to print alphabets
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int c;
+	{
+		putchar(alp(c));
+	}
+	putchar("\n");
+	return (0);
+}
