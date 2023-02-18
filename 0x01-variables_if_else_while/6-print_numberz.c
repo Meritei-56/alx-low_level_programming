0e4caebce8b0cfdea9ba83f8889a9cfffe5c761e
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-17 18:58:03 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program to print base 10
+ * numbers starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
