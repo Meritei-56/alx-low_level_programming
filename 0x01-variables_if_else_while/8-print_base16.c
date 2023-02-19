73234414c84c5b9f64d128c8e2ccf8eb1c0ac760
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-19 13:35:39 -0800

    Task 8 C Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a programto print base 16 numbers in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int h;
+	char d;
+
+	for (h = '0'; h <= '9'; h++)
+	{
+		putchar(h);
+	}
+	putchar('\n');
+
+	for (d = 'a'; d <= 'f'; d++)
+	{
+		putchar(d);
+	}
+	return (0);
+}
