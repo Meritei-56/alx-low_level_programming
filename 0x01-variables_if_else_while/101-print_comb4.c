e28c02d96698ed0a0146087d119acc77a3c0fa12
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-19 22:32:45 -0800

    Task 11 C Variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program to print different combinations of three digits
+ * only the smallest combinations to be printed
+ * only putchar function to be used
+ * putchar function to be used 6 times maximum
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+	int n;
+	int o;
+
+	for (m = '0'; m < '9'; m++)
+	{
+	for (n = m + 1; n <= '9'; n++)
+	{
+	for (o = n + 1; 0 <= '9'; o++)
+	{
+	if ((m != n) != o)
+	{
+	putchar(m);
+	putchar(n);
+	putchar(o);
+	if (m == '7' && n == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
