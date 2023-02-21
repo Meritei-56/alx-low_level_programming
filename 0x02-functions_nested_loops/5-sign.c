9f797936945cc8ea51e050ed7514abdfd89e3dd4
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 12:14:26 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - create code
- * print_sign: a function in file main.h to print sign of numbers
+ * print_sign: prototype function defined in file main.h to print sign of numbers
  *
  * @n: numbers entered for the system to print their signs
  *
