8493f94ac499ec80dc7194a29d4f6c60596f4122
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 08:11:10 -0800

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,18 +3,18 @@
 /**
  * int _islower(int c) - prototype
  * _islower - program to detect lowercase numbers
- * c _islower - user program
+ *'c' _islower - user program
  *
  * Return: 1 if c is lowercase or 0 if otherwise
  */
 
 int _islower(int c)
 {
-	char c;
-	
-	for (al = '97' && al <= '122')
-	{
-	return (1);
-	}
-	return (0);
+
+if (c = '97' && c <= '122')
+{
+
+return (1);
+}
+return (0);
 }
