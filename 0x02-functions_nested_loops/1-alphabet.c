83714200129748768f1f6cd4063d33e7c70ba1a1
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 04:17:51 -0800

    Task 1 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - function to print the alphabets in lowercase
+ * followed by new line
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
