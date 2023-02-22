3106f8f33113e7a55b76317bce3d42df89400de6
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 17:36:25 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * _abs(int): returns absolute value of integers
+ * int: integers are entered and computed
+ *
+ * Return: absolute values of integers
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
