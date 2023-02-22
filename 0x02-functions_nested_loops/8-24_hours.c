ba000183eab8fc87be23746aeba961057457c917
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 18:55:16 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print every minute of Jack Bauer day
+ * jack_bauer: program to calculate every minute of the day
+ * @m: minutes
+ * @h: hours
+ */
+
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(',');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
