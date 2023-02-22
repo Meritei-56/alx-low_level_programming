3e45e6235ce847163ee76b783de6ff4c73f993fb
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-21 18:32:45 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * code to compute sum of two integers
+ *
+ * add: computes sums of numbers
+ *
+ * @a and @b: integers
+ * Return: sum of integers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
