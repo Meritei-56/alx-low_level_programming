6ae4139b580bd477cac0a0b16ac86438f4010686
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-23 08:35:47 -0800

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,16 +1,15 @@
 #include "main.h"
 #include "stdio.h"
 /**
- * mul: finds the product of two integers
+ * mul - definition of function
+ * Description: multiplication of two integers
  * @m: integer
  * @n: integer
  * Return: multiplication of two integers
  */
 int mul(int m, int n)
 {
-	int mult;
-
-	mult = m * n;
+	int mult = m * n;
 
 	return (mult);
 }
