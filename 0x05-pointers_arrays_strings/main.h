e0c22578eba62cf22669a1a76026ac6f98c9ad73
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-27 17:16:58 -0800

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
-int _putchar(char *c);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *srn);
 void _puts(char *srn);
+char _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
