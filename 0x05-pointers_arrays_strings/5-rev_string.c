6004e72f64e6d243f0e61e0097d4db6140324a32
Meritei-56 <shadrackmeritei@gmail.com>
2023-02-27 18:32:47 -0800

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function
+ * Description: reverses string
+ * @t: string
+ * Return: void
+ */
+void rev_string(char *t)
+{
+	int k = 0;
+	char r = t[0];
+	int l;
+
+	while (t[k] != '\0')
+	k++;
+	for (l = 0; l < k; l++)
+	{
+		k--;
+		r = t[l];
+	       t[k] = r;
+	}
+}	
