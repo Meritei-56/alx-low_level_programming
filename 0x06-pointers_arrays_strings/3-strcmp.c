b153007fcfa416c3f1e9ce8a56db42470332100d
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-02 01:25:29 -0800

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,22 +11,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int k;
 
-       	k = 0;
+	k = 0;
 	while (s1[k] != '\0' && s2[k] != '\0')
 	{
-	k++;
+		if (s1[k] != s2[k])
+		{
+			return (s1[k] - s2[k]);
+		}
+		k++;
 	}
-	if (s1[k] == s2[k])
-	{
-	k++;
-	}
-
-	else
-	{
-	return (s1[k] - s2[k]);
-	}
-	k++;
-
 	return (0);
 }
 
