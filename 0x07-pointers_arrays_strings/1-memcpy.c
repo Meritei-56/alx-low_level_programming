3b0406a35c2ac8b3ae45974c4f80e2d6336e27e6
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-06 06:12:30 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - function definition
+ * Description: copies memory area
+ * @src: memory area
+ * @dest: memory area
+ * @n: bytes to be copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int k = 0;
+	int j;
+	
+	for (; k < j; k++)
+	{
+		src[k] = dest[k];
+		n--;
+	}
+	return (dest);
+}
