2316beba1d278bbcc68c8e842bcc819cd68475b5
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-25 06:15:38 -0700

    Ammended to cmply with betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,7 +5,6 @@
  * Description: prints name
  * @name: person's name
  * @f: function that returns void
- * @char: char pointer argument for f function
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
