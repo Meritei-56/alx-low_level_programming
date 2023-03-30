ceca38e74abdc3c9ca84c9bb8c9336e4f705805d
Meritei-56 <shadrackmeritei@gmail.com>
2023-03-30 16:19:03 -0700

    Prints a sentence

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_sentence - function
+ * Description: prints sentences
+ *
+ * Return: void
+ */
+void print_sentence(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+
+}
