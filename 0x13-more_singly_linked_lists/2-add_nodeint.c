bc4259408781948e0ad7d56fecf68a36ca81a12a
Meritei-56 <shadrackmeritei@gmail.com>
2023-04-04 00:52:21 -0700

    Function ammended t align with checker

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,19 +14,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *ptr;
 
 	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+
 	ptr->n = n;
 	ptr->next = *head;
 
 	*head = ptr;
-
-	if (ptr == NULL)
-		return (NULL);
-
-
-	while (ptr != NULL)
-	{
-	ptr = ptr->next;
-	}
 
 	return (ptr);
 }
